bdc7a881ce50101a7d347bfc1e5eeeffbd040880
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-06 15:39:19 +0100

    Add styles to AddTodo component

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -5,7 +5,7 @@ const newTodo = ref('');
 const emits = defineEmits(['add-todo']);
 
 const submitTodo = () => {
-    if(newTodo.value.trim() !== '') {
+    if (newTodo.value.trim() !== '') {
         emits('add-todo', newTodo.value);
         newTodo.value = '';
     }
@@ -17,7 +17,37 @@ const submitTodo = () => {
 
 <template>
     <div class="add-todo">
-        <input v-model="newTodo" @keyup.enter="submitTodo" placeholder="Add a new todo"/>
+        <input v-model="newTodo" @keyup.enter="submitTodo" placeholder="Add a new todo" />
         <button @click="submitTodo">Add</button>
     </div>
 </template>
+
+<style>
+.add-todo {
+    display: flex;
+    margin-bottom: 1rem;
+}
+
+.add-todo input {
+    flex: 1;
+    padding: 0.5rem;
+    font-size: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+.add-todo button {
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    margin-left: 0.5rem;
+    background-color: #42b983;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.add-todo button:hover {
+    background-color: #38a373;
+}
+</style>
