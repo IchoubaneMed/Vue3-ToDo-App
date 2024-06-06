a91e6d15a3bda480630490524e0df45ea4384135
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-06 15:43:56 +0100

    Add template and script to TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+const props = defineProps({
+    todo: String,
+});
+
+const emit = defineEmits(['delete-todo']);
+
+const deleteTodo = () => {
+    emits('delete-todo');
+};
+
+</script>
+
+
+<template>
+    <li class="todo-item">
+        {{ todo }}
+        <button @click="deleteTodo">Delete</button>
+    </li>
+</template>
