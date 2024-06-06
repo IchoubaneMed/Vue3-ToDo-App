a2838c9b99c0f3ac26e3b4ac397b7637a01d4d8a
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-06 16:13:37 +0100

    Add template, script, and style to TodoList component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import TodoItem from './TodoItem.vue';
+
+const props = defineProps({
+    todos: Array,
+})
+
+const emits = defineEmits(['delete-todo']);
+
+const deleteTodo = (index) => {
+    emits('delete-todo', index)
+};
+</script>
+
+
+
+<template>
+    <ul class="todo-list">
+        <TodoItem v-for="(todo, index) in todos" :key="index" :todo="todo" @delete-todo="deleteTodo"/>
+    </ul>
+</template>
+
+<style scoped>
+    .todo-list {
+        list-style: none;
+        padding: 0;
+    }
+</style>
