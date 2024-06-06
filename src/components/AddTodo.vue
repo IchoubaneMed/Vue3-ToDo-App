8eddbaa673212113b5e719549879d614fb89a3f2
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-06 15:32:30 +0100

    Add script to AddTodo component

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,3 +1,20 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const newTodo = ref('');
+const emits = defineEmits(['add-todo']);
+
+const submitTodo = () => {
+    if(newTodo.value.trim() !== '') {
+        emits('add-todo', newTodo.value);
+        newTodo.value = '';
+    }
+};
+
+
+</script>
+
+
 <template>
     <div class="add-todo">
         <input v-model="newTodo" @keyup.enter="submitTodo" placeholder="Add a new todo"/>
