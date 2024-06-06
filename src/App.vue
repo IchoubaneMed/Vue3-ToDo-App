b04439df07ffd7279bbee9c12d189b333cf6720b
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-06 16:12:54 +0100

    Add template and script to src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,33 @@
-<template>
+<script setup>
+import { ref } from 'vue';
 
-  <h1>Hello, World</h1>
+import AddTodo from './components/AddTodo.vue';
+import TodoList from './components/TodoList.vue';
+
+import { useToast } from 'vue-toastification';
+
+const todos = ref([]);
+const toast = useToast();
+
+const AddTodo = (todo) => {
+  todo.value.push(todo);
+  toast.success('Todo added!');
+};
+
+const deleteTodo = (index) => {
+  todos.value.splice(index, 1);
+  toast.error('Todo deleted!');
+}
+
+</script>
+
+
+<template>
+  <header>
+    <h1>Vue 3 ToDo App</h1>
+  </header>
+  <main>
+    <AddTodo @add-todo="AddTodo" />
+    <TodoList :todos="todos" @delete-todo="deleteTodo"/>
+  </main>
 </template>
