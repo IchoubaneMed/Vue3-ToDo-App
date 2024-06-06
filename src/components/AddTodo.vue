bdc51093caf03f66d0b8aef263e2397d9a1108f4
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-06 15:28:49 +0100

    Add template elements to AddTodo component

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="add-todo">
+        <input v-model="newTodo" @keyup.enter="submitTodo" placeholder="Add a new todo"/>
+        <button @click="submitTodo">Add</button>
+    </div>
+</template>
