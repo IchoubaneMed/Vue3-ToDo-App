90112257c2b779cad7aa8be5b5a1aebd921522a3
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-07 17:52:33 +0100

    add TodoListCreate, TodoDetail, CategoryListCreate, and CategoryDetail views

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Todo, Category
+from .serializers import TodoSerializer, CategorySerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)  
+
+class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+
+
+
