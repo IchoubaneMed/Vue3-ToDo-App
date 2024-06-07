d1616b1f3303df8b93cbb828062efe13ba3df5f3
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-07 11:48:09 +0100

    define the models for Category and Todo

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=50, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
