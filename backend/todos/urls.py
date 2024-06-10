5eca7f78ed148c9ca6a323d2a007b9c12e98f01f
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-10 14:58:21 +0100

    Add urls for user registration and retrieval endpoints

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TodoListCreate, TodoDetail, CategoryListCreate, CategoryDetail
+from .views import TodoListCreate, TodoDetail, CategoryListCreate, CategoryDetail, RegisterView, UserDetailView
 
 urlpatterns = [
     path('todos/', TodoListCreate.as_view(), name='todo-list-create'),
     path('todos/<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
     path('categories/', CategoryListCreate.as_view(), name='category-list-create'),
     path('categories/<int:pk>/', CategoryDetail.as_view(), name='category-detail'),
+    path('auth/users/', RegisterView.as_view(), name='register'),
+    path('auth/users/me/', UserDetailView.as_view(), name='user-detail'),
 ]
