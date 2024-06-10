053da4f3edee54a3b022eb65315498b8e609de32
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-10 11:04:42 +0100

    Install axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
 import './assets/styles.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import App from './App.vue'
 
 const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia)
 app.use(Toast)
 app.mount('#app')
