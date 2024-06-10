802a854382c5118562090312863ef0223a58e5ed
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-10 14:54:12 +0100

    Add Axios configuration that includes the authentication token and base URL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,18 @@
 import './assets/styles.css'
-
+import axios from 'axios';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import App from './App.vue'
+
+axios.defaults.baseURL = 'http://localhost:8000';
+
+const token = localStorage.getItem('token');
+
+if(token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 const app = createApp(App);
 const pinia = createPinia();
