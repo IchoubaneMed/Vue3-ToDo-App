e4389bc5425389d8d17e09e3ee2aaf8ebf1fbe01
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-10 14:57:16 +0100

    Add views to handle user registration and retrieval

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,10 +1,23 @@
 from rest_framework import generics
+from django.contrib.auth.models import User
 from .models import Todo, Category
-from .serializers import TodoSerializer, CategorySerializer
-from rest_framework.permissions import IsAuthenticated
+from .serializers import TodoSerializer, CategorySerializer, UserSerializer, RegisterSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+class UserDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
 
 class TodoListCreate(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
@@ -22,6 +35,9 @@ class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
 
 class CategoryListCreate(generics.ListCreateAPIView):
     queryset = Category.objects.all()
